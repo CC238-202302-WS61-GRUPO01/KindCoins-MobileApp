c6ebe592194a22b9b4e4629041c3ece37303ca52
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-09-28 03:40:14 -0500

    Añadido de las vistas del código
    Se puso la vista de inserción del código
    Se implementó la tercera página, no completamente.

--- kindcoins/lib/main.dart
+++ kindcoins/lib/main.dart
@@ -10,8 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Kind Coins",
-      //home: Inicio(),
-      home: RegistroA(),
+      home: Inicio(),
     );
   }
 }
@@ -45,28 +44,146 @@ class RegistroA extends StatefulWidget {
 
 class _RegistroAState extends State<RegistroA> {
   TextEditingController phoneController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Registro"),
         ),
-        body: Container(
-            padding: const EdgeInsets.all(20),
-            child: ListView(
-              children: [
-                const Text("Número de celular"),
-                const SizedBox(
-                  height: 5,
-                ),
-                TextField(
-                  controller: phoneController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
-            )));
+        body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(25),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const Text("Número de celular"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese su celular',
+                      ),
+                    ),
+                    //Espacio extra
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    //Espacio extra
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ValidarCodigo()));
+                        },
+                        child: const Text('Enviar Código')),
+                  ],
+                ))));
+  }
+}
+
+class ValidarCodigo extends StatefulWidget {
+  const ValidarCodigo({super.key});
+
+  @override
+  State<ValidarCodigo> createState() => _ValidarCodigoState();
+}
+
+class _ValidarCodigoState extends State<ValidarCodigo> {
+  TextEditingController codigoController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Registro"),
+        ),
+        body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(25),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const Text("Código de confirmación"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: codigoController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese el código',
+                      ),
+                    ),
+                    //Espacio extra
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    //Espacio extra
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistroB()));
+                        },
+                        child: const Text('Validar')),
+                  ],
+                ))));
+  }
+}
+
+class RegistroB extends StatefulWidget {
+  const RegistroB({super.key});
+
+  @override
+  State<RegistroB> createState() => _RegistroBState();
+}
+
+class _RegistroBState extends State<RegistroB> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController birthdayController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Registro"),
+        ),
+        body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(25),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const Text("Número de celular"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese su celular',
+                      ),
+                    ),
+                    //Espacio extra
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    //Espacio extra
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ValidarCodigo()));
+                        },
+                        child: const Text('Enviar Código')),
+                  ],
+                ))));
   }
 }
