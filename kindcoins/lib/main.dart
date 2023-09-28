440ecdef87e3ca4cd9f7fbf1b99982f0a89863f4
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-09-28 02:56:06 -0500

    Commt 1
    Código de la primera página del registro... Solo código, nada visual.

--- kindcoins/lib/main.dart
+++ kindcoins/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -31,5 +32,36 @@ class _InicioState extends State<Inicio> {
         body: Center(
           child: Text("Contenido"),
         ));
+  }
+}
+
+class RegistroA extends StatefulWidget {
+  const RegistroA({super.key});
+
+  @override
+  State<RegistroA> createState() => _RegistroAState();
+}
+
+class _RegistroAState extends State<RegistroA> {
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Registro"),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Text("Número de celular"),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            )));
   }
 }
