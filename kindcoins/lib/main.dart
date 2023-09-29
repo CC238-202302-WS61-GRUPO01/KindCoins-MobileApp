839de65929bf0e0921c5aa1f86cf57783214fea8
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-09-28 21:34:20 -0500

    Añadidos del 28/02/23
    Registro B: Aañadido controladores para insertar texto, añadido de date picker para la fecha de nacimiento, Añadido de los elementos del formulario, añadido de dropdown para el género... aún se requiere poder modificar el valor del género

--- kindcoins/lib/main.dart
+++ kindcoins/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+
 
 void main() => runApp(MyApp());
 
@@ -144,9 +144,23 @@ class RegistroB extends StatefulWidget {
 
 class _RegistroBState extends State<RegistroB> {
   TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController birthdayController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  String genderValue = 'Indefinido';
+
+  DateTime selectedDate = DateTime.now();
+
+  void _showDatePicker() {
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2015)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -159,6 +173,20 @@ class _RegistroBState extends State<RegistroB> {
                 child: ListView(
                   shrinkWrap: true,
                   children: [
+                    const Text("Nombre"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese su nombre',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     const Text("Número de celular"),
                     const SizedBox(
                       height: 10,
@@ -168,6 +196,86 @@ class _RegistroBState extends State<RegistroB> {
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Ingrese su celular',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text("Correo Electrónico"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese su correo electrónico',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text("Fecha de nacimiento ${selectedDate}"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                        child: Text("Elija la fecha"),
+                        onPressed: () async{
+                          final DateTime? dateTime = await showDatePicker(
+                              context: context,
+                              initialDate: selectedDate,
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2050)
+                          );
+                          if (dateTime != null){
+                            setState(() {
+                              selectedDate = dateTime;
+                            });
+                          }
+                        }
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text("Género"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    DropdownButton<String>(
+                      items: const[
+                        DropdownMenuItem(
+                            value: 'No binario',
+                            child: Text('no binario')
+                        ),
+                        DropdownMenuItem(
+                            value: 'Masculino',
+                            child: Text('Masculino')
+                        ),
+                        DropdownMenuItem(
+                            value: 'Femenino',
+                            child: Text('Femenino')
+                        )
+                      ],
+                      onChanged: (String? newValue){
+                        setState(() {
+                          genderValue=newValue!;
+                        });
+                      },
+                    ),
+                    //Espacio extra
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text("Dirección"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: addressController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Ingrese su dirección',
                       ),
                     ),
                     //Espacio extra
@@ -180,7 +288,7 @@ class _RegistroBState extends State<RegistroB> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => ValidarCodigo()));
+                                  builder: (context) => Inicio()));
                         },
                         child: const Text('Enviar Código')),
                   ],
