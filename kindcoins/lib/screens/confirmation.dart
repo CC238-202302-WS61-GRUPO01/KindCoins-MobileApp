457b201df96421580ec24301ecd7552bc38ef9c2
Markollantes2307 <u201410183@upc.edu.pe>
2023-10-23 20:28:53 -0500

    fix: last alertDialog configs

--- kindcoins/lib/screens/confirmation.dart
+++ kindcoins/lib/screens/confirmation.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class Confirmation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Text("Confirmación"),
-            Spacer(
-              flex: 1,
-            ),
-            Text("Pago realizado con éxito")
-          ],
+    return AlertDialog(
+      title: Text("Confirmación"),
+      content: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Spacer(
+                flex: 1,
+              ),
+              Text("Pago realizado con éxito")
+            ],
+          ),
         ),
       ),
     );
